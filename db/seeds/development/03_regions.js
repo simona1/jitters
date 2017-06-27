c2cd1b8fae02f3b29955810a8a3223548442fbb2
Kurtis Houser <houserkurtis@gmail.com>
2017-06-27 10:40:53 -0700

    Add additional seed data and update tests

--- db/seeds/development/03_regions.js
+++ db/seeds/development/03_regions.js
@@ -12,6 +12,33 @@ exports.seed = function(knex, Promise) {
           long: 38.2058150,
           created_at: new Date('2017-06-23 14:56:16 UTC'),
           updated_at: new Date('2017-06-23 14:56:16 UTC')
+        },
+        {
+          id: 2,
+          country_id: 2,
+          name: 'Oromia',
+          lat: 7.546038,
+          long: 40.6346851,
+          created_at: new Date('2017-06-23 14:56:16 UTC'),
+          updated_at: new Date('2017-06-23 14:56:16 UTC')
+        },
+        {
+          id: 3,
+          country_id: 3,
+          name: 'Bruselas-Huila',
+          lat: 1.776552,
+          long: -76.176076,
+          created_at: new Date('2017-06-23 14:56:16 UTC'),
+          updated_at: new Date('2017-06-23 14:56:16 UTC')
+        },
+        {
+          id: 4,
+          country_id: 3,
+          name: 'Popayán',
+          lat: 2.4448143,
+          long: -76.6147395,
+          created_at: new Date('2017-06-23 14:56:16 UTC'),
+          updated_at: new Date('2017-06-23 14:56:16 UTC')
         }])
         .then(() => {
           return knex.raw("SELECT setval('regions_id_seq', (SELECT MAX(id) FROM regions));");
